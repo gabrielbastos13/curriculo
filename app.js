8355e490af131937aaf203195ba1d1dd5385c41c
Gabriel Gonçalves <gabriel.bastos13@hotmail.com>
2019-12-17 08:41:10 -0300

    subindo o curriculo express

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+const port = 3000;
+const path = require('path');
+const CurriculoController = require('./controllers/curriculo-controller');
+const createError = require('http-errors');
 
-app.get('/', (req, res, next) => {
-	res.send({
-	  title: "Meu primeiro servidor Express",
-	  version: "0.0.0"
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res, next) {
+	res.render('index', {
+		title: "Meu primeiro servidor Express",
+		version: "0.0.0"
 	});
 });
 
+app.get('/curriculo',(req, res, next) => {
+	const curriculoData = CurriculoController.getData();
+    res.render('curriculo', curriculoData);
+});
 
 app.listen(port, err => {
-	console.log('Server is listening on ${port}');
+	console.log(`Server is listening on ${port}`);
 });
+
+
 
---
+++ models/curriculo-model.js
@@ -0,0 +1,19 @@
+		
+	const data = {
+		title: 'Meu currículo',
+        name: 'Gabriel Gonçalves Bastos',
+        profession: 'Front End Developer',
+        description: 'Estudante assíduo em busca de sua primeira oportunidade no mercado de trabalho.',
+        experience: [{
+            company: 'Movile',
+            office: 'Motoboy',
+            description: 'Trabalho na outra ponta da tecnologia, recebendo cordenadas e tarefas de um algoritmo, neste momento decidi entrar de vez no mundo da tecnologia'
+        }],
+        education: [{
+            institution: 'Vida',
+            description: 'Cachorro e louco do asfalto, e MBA em pegar atalhos no centro e ruas da capital'
+        }],
+        skills: ['backend', 'frontend']
+	}
+	
+	module.exports = data;
